ced15b0d771ddb14e2cde8d196da904483d71e80
MoonSky1! <vanmathyrr@gmail.com>
2023-09-06 14:13:47 -0400

    Default Language added to Preferred Language field in the property field

--- src/webparts/reactFaq/loc/fr-fr.js
+++ src/webparts/reactFaq/loc/fr-fr.js
@@ -1,13 +1,14 @@
 define([], function() {
   return {
-    "PropertyPaneDescription": "Description",
-    "BasicGroupName": "Group Name",
-    "DescriptionFieldLabel": "Description Field",
-    "ListNameFieldLabel": "List Name",
-    "Lang": "FR",
-    "searchLabel":"Entrée de recherche",
-    "placeholderSearch": "Recherche",
-    "iconPlusLabel": "Icon plus",
-    "iconMinusLabel": "Icon moins"
-  }
+    userLang: "fr-fr",
+    PropertyPaneDescription: "Description",
+    BasicGroupName: "Group Name",
+    DescriptionFieldLabel: "Description Field",
+    ListNameFieldLabel: "List Name",
+    Lang: "FR",
+    searchLabel: "Entrée de recherche",
+    placeholderSearch: "Recherche",
+    iconPlusLabel: "Icon plus",
+    iconMinusLabel: "Icon moins",
+  };
 });
